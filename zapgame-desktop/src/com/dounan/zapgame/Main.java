2bee8fb49d8da86e4c82b2ed02c84e1d9e3d8a89
Dounan Shi <dounan@gmail.com>
2013-04-25 23:04:52 -0700

    Initial libgdx project generation

---
+++ zapgame-desktop/src/com/dounan/zapgame/Main.java
@@ -0,0 +1,16 @@
+package com.dounan.zapgame;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "zapgame";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new Zap(), cfg);
+	}
+}
